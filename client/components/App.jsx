7ca3980f88d3c5e007abd265c9654a3c66a83327
Sungmiii <christyksmile@gmail.com>
2018-11-22 16:19:22 +1300

    drop drag figured out how to do it by lord James

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div style={{ position: "absolute" }}>
+      <h1>React development has begun!</h1>
+
+      <p id="redBox" style={{ position: "absolute", top: "100px", left: "400px", background: 'red', width: "100px", height: "100px" }} draggable="true" onDragEnd={dropped} onMouseMove={mouseMove} onDrop={dragDrop} onDrag={aFunction}>a</p>
+    </div>
   )
+}
+
+
+var xpos = Math.floor(Math.random() * 500);
+
+var ypos = Math.floor(Math.random() * 500);
+
+function aFunction(event) {
+  // console.log(event.clientX)
+  ypos = event.clientX;
+  xpos = event.clientY;
+}
+
+function dropped(event) {
+  console.log("dropped at x" + xpos + " y" + ypos)
+
+  document.getElementById("redBox").style["left"] = event.clientX + "px";
+  document.getElementById("redBox").style["top"] = event.clientY + "px";
+}
+
+function mouseMove(event) {
+  // console.log("mouse move ", event.clientX)
+
+}
+
+function dragDrop(event) {
+  // console.log('dragDrop', event.clientX)
 }
 
 export default App
