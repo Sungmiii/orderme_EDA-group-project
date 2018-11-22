042cfcf9ecd024c1e07a244f1e4236ce2344ba46
Garry <garry@garry.bar>
2018-11-23 10:03:54 +1300

    component resize

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,10 +3,9 @@ import GameSpace from './GameSpace'
 
 const App = () => {
   return (
-    <div style={{ position: "absolute" }}>
-      <h1>React development has begun!</h1>
+    <div className="mainapp" style={{ position: "absolute" }}>
 
-      <p id="redBox" style={{ position: "absolute", top: "100px", left: "400px", background: 'red', width: "100px", height: "100px" }} draggable="true" onDragEnd={dropped} onMouseMove={mouseMove} onDrop={dragDrop} onDrag={aFunction}>a</p>
+
 
       <GameSpace />
     </div>
@@ -14,31 +13,8 @@ const App = () => {
 }
 
 
-var xpos = Math.floor(Math.random() * 500);
 
-var ypos = Math.floor(Math.random() * 500);
 
-function aFunction(event) {
-  // console.log(event.clientX)
-  ypos = event.clientX;
-  xpos = event.clientY;
-}
-
-function dropped(event) {
-  console.log("dropped at x" + xpos + " y" + ypos)
-
-  document.getElementById("redBox").style["left"] = event.clientX + "px";
-  document.getElementById("redBox").style["top"] = event.clientY + "px";
-}
-
-function mouseMove(event) {
-  // console.log("mouse move ", event.clientX)
-
-}
-
-function dragDrop(event) {
-  // console.log('dragDrop', event.clientX)
-}
 
 export default App
 
--- client/components/GameSpace.jsx
+++ client/components/GameSpace.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import Letter from './alphabet/A'
 
 export default function GameSpace() {
     return (
-        <div>
+        <div className="gamespace">
             <Header />
+            <Letter class="letterBlock" />
             <Footer />
         </div>
     )
